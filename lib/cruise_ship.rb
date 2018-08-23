d99533ff7b318fc0101280fba712db8ff84d2498
Kim Pham <pham.kim.a@gmail.com>
2018-08-23 20:05:49 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,5 +8,8 @@
 
 def select_winner(passengers)
   passengers.each do |key, value|
-    if key == :suite_a && value
+    if key == :suite_a && value[0] == "A"
+      return  value
+    end
+  end
 end
